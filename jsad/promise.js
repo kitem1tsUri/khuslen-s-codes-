770b9e226ad22eb7980c110727551c7730c37d01
kitem1tsUri <huslenbasketball@gmail.com>
2022-09-12 19:01:55 +0800

    glg resolve mom ysnch gechiv

---
+++ jsad/promise.js
@@ -0,0 +1,27 @@
+let willIGetNewPhone = new Promise((resolve, reject) => { 
+    setTimeout(() => { 
+        let isMomhappy = Math.round(Math.random()); 
+        if  (isMomhappy) { 
+            let phone = { 
+                brand: "iPHone 14",
+                color: "purple",
+            
+            }; 
+            resolve(phone);
+        } else { 
+            let reason = new Error ("mom is not happy");
+            reject(reason); 
+        }
+    } , 2000)
+})
+
+let askMom = () => { 
+    return willIGetNewPhone
+    .then((resolve) => { 
+        console.log(resolve); 
+
+    })
+    .catch((reject) => { 
+        console.log(reject)
+    })
+}
