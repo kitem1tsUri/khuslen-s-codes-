218cd60a838d19dcdac0ae51ded4ca330111f9b5
kitem1tsUri <huslenbasketball@gmail.com>
2022-09-16 19:08:09 +0800

    sda

--- jsad/promice.js
+++ jsad/promice.js
@@ -1,8 +1,8 @@
+let i=11
 const asuu = async () => {
     const response = await fetch("https://www.randomuser.me/api/?results=50");
-    
    const data = await response.json();
-  console.log(data.emails);
+  console.log(data.results[emails].emails);
   document.getElementById("asu").innerHTML = data;
 } 
   
---
+++ jsad/stopwatch.js
@@ -0,0 +1,6 @@
+
+const element = document.getElementById("demo");
+setInterval(function() {element.innerHTML += "Hello"}, 1000); 
+const dooli = () =>  { 
+setInterval(function())
+}
