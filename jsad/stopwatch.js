a02e72f6f262fdddfc116777790e36a50b658b74
kitem1tsUri <huslenbasketball@gmail.com>
2022-09-19 19:09:11 +0800

    weather ks

--- jsad/stopwatch.js
+++ jsad/stopwatch.js
@@ -1,6 +1,16 @@
 
-const element = document.getElementById("demo");
-setInterval(function() {element.innerHTML += "Hello"}, 1000); 
-const dooli = () =>  { 
-setInterval(function())
+let element = document.getElementById("tens");
+const start = () => {
+setInterval(function() {element.innerHTML = Number( element.innerHTML) + Number(1)}, 10);
+  switch (element.innerHTML) {
+    case Number(100) :
+      console.log("a");
+      break;
+} 
+
 }
+//   if (element.innerHTML === 100) { 
+//     console.log("a")
+//     // document.getElementById("seconds").innerHTML = Number(document.getElementById("seconds").innerHTML) + Number(1)
+//     // element.innerHTML = 0 
+// } else { console.log("b")}
